e7e9a813eb6a6adf9d86b1287fea80968749f34f
Khaja <moinuddinbtech@gmail.com>
2015-06-28 11:06:52 -0500

    Initial Commit - Adding the project to the GitHub.

---
+++ GitFirstProject/src/com/techm/testpack1/TestClass1.java
@@ -0,0 +1,12 @@
+package com.techm.testpack1;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	
+	@Test
+	public void helloWorld(){
+		System.out.println("Hello World!!!");
+	}
+
+}
